7773ee7bb92d512963f129447564627ff1848886
Hakan Kara <hakanbatirhan@gmail.com>
2023-10-14 15:33:17 +0300

    Update Runner.java

--- src/test/java/mini_bootcamp_cucumber/runners/Runner.java
+++ src/test/java/mini_bootcamp_cucumber/runners/Runner.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
         },
         monochrome = true,//raporlarin consoleda okunakli sekilde cikmasi icin
         features = "src/test/resources/features",//features folder path
-        glue = {"mini_bootcamp_cucumber/stepdefinitions", "hooks"},   //stepdefinitions path
+        glue = {"mini_bootcamp_cucumber/stepdefinitions"},   //stepdefinitions path
         tags = "@ets",
         dryRun = false
 )
