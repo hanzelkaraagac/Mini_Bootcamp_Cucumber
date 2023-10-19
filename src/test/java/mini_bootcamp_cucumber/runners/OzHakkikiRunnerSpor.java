bdc7649f65e0fa5e1f93e6d2851935221fd2619f
HakanKara75 <hakanbatirhan@gmail.com>
2023-10-19 22:55:35 +0300

    v

--- src/test/java/mini_bootcamp_cucumber/runners/OzHakkikiRunnerSpor.java
+++ src/test/java/mini_bootcamp_cucumber/runners/OzHakkikiRunnerSpor.java
@@ -8,11 +8,11 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         plugin = {
                 "pretty",//raporlarin daha ikunakli olmasi icin
-                "html:target/default-cucumber-reports.html",
+                "html:target/default-cucumber-reports1.html",
                 "json:target/json-reports/cucumber1.json",  //plugin ekledikten sonra bunu cucumber report için ekle. Paralel test icin dosya adina 1 rakami ekle
-
+                "junit:test-output/htmlReport/cucumber1.xml", //xml report için
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:", //pdf spark report icin
                 "rerun:Failed/failedScenarios.txt" // bu satirdaki kod kosulan testlerden hata verenlerin yeniden ReRunner classi tarafindan kosulmasini saglar
-
         },
         monochrome = true,//raporlarin consoleda okunakli sekilde cikmasi icin
         features = "src/test/resources/features",//features folder path
@@ -26,3 +26,4 @@ public class OzHakkikiRunnerSpor {
 //Bu sinif Test Caseleri run etmek icin kullanilir
 
 //Runners class, feature file lar ile step definition'i birbirine baglar
+
